d239d85cb0fdd612abadfdff8190caf1ba36e334
Roterfux <244@mailbox.org>
2022-05-04 15:25:16 +0200

    small changes

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ def update():
         for entity in moons:
             entity.rotation_y -= time.dt * 100
 
+    diff = sqrt((sol[0].x - mouse.x) ** 2 + (sol[0].y - mouse.y) ** 2 + sol[0].z ** 2)
+    #print(diff)
+    diff_x = sol[0].x - mouse.x
+    print(diff_x)
+
     key_action()
     mouse_action()
-    print("X diff: ", sol[0].x - mouse.x)
-    print("Y diff: ", sol[0].y - mouse.y)
-    #print(sol[0].z -calc)
 
 
 def key_action():
@@ -36,17 +38,15 @@ def key_action():
 
     if held_keys['x']:
         exit(0)
-    # print("{:.3f} {:.3f} {:.3f}".format(camera.position.x, camera.position.y, camera.position.z))
 
 
 def mouse_action():
     if mouse.hovered_entity == sol[0]:
         info.visible = True
-        info.x = mouse.x
-        info.y = mouse.y
+        info.x = mouse.x + .025
+        info.y = mouse.y - .025
     else:
         info.visible = False
-    # print(": {:.3f} {:.3f}".format(mouse.x, mouse.y))
 
 
 def input(key):
@@ -68,51 +68,24 @@ def input(key):
 
 
 def star():
-    sol = Entity(model='sphere',
-                 position=(0, 0, 0),
-                 color=color.yellow,
-                 scale=2,
-                 texture="tex/2k_sun",
-                 collider="sphere")
-    corona = Entity(model='sphere',
-                    position=(0, 0, 0),
-                    color=color.rgba(255, 255, 0, 128),
-                    scale=2.05,
-                    texture="tex/2k_sun")
-    corona2 = Entity(model='sphere',
-                     position=(0, 0, 0),
-                     color=color.rgba(255, 255, 0, 32),
-                     scale=2.15)
+    sun = Entity(model='sphere', position=(0, 0, 0), color=color.yellow, scale=2, texture="tex/2k_sun", collider="sphere")
+    corona = Entity(model='sphere', position=(0, 0, 0), color=color.rgba(255, 255, 0, 128), scale=2.05, texture="tex/2k_sun")
+    corona2 = Entity(model='sphere', position=(0, 0, 0), color=color.rgba(255, 255, 0, 32), scale=2.15)
 
-    return [sol, corona, corona2]
+    return [sun, corona, corona2]
 
 
 def planet(parent, position, texture):
-    sphere = Entity(parent=parent,
-                    model='sphere',
-                    position=position,
-                    scale=0.2,
-                    texture=texture)
-    sphere_atmosphere = Entity(parent=parent,
-                               model='sphere',
-                               position=position,
-                               color=color.rgba(0, 0, 128, 96),
-                               scale=.22,
-                               collider='box',
-                               on_click=show_name)
+    sphere = Entity(parent=parent, model='sphere', position=position, scale=0.2, texture=texture)
+    sphere_atmosphere = Entity(parent=parent, model='sphere', position=position, color=color.rgba(0, 0, 128, 96), scale=.22, collider='box', on_click=show_name)
+
     return [sphere, sphere_atmosphere]
 
 
 def moon(parent):
     for _ in range(10):
-        satellite = Entity(parent=parent,
-                           model='sphere',
-                           color=color.white,
-                           position=(1 + random.random() * 1.3, 0, 0),
-                           scale=0.1 * random.random(),
-                           texture="tex/2k_moon",
-                           rotation_y=random.random() * 180
-                           )
+        satellite = Entity(parent=parent, model='sphere', color=color.white, position=(1 + random.random() * 1.3, 0, 0), scale=0.1 * random.random(), texture="tex/2k_moon",
+                           rotation_y=random.random() * 180)
     return [satellite]
 
 
@@ -129,11 +102,9 @@ def show_name():
 def setup():
     camera.position = (0, 3.7, -7.5)  # x, y, z
     camera.rotation_x = 30
-    window.borderless = False  # Show a border
-    window.fullscreen = False  # Go full screen
+    # window.fullscreen = False  # Go full screen
     window.exit_button.visible = True  # Show the in-game red X that loses the window
     window.fps_counter.enabled = True  # Show the FPS (Frames per second) counter
-    window.size = (1200, 800)
     window.center_on_screen()
     window.color = color.rgba(10, 10, 10, 0)
 
@@ -158,8 +129,8 @@ planet = planet(sol[0], position=(1, 0, 1), texture="tex/earth")
 moons = moon(planet[0])
 
 db_test()
-
 info = Text(text="This is a test", color=color.white, scale=1)
+info.background = True
 
 setup()
 
